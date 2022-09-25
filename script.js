4d42721ef92d4e28fff8d85514417b7ad12e8157
Joel <joel.knoll1@gmail.com>
2022-09-24 21:25:09 -0400

    Draw, erase, and clear buttons operational.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let canvasSize = 64;
-let drawing; 
+let drawing = false;
+let erasing = false; 
 let canvas = document.getElementById("canvas-container");
 let clearBtn = document.getElementById("clear");
+let drawBtn = document.getElementById("draw");
+let eraseBtn = document.getElementById("erase");
+
 
 for(let i = 0; i < canvasSize; i++){
   let newRow = document.createElement("div");
@@ -9,9 +13,14 @@ for(let i = 0; i < canvasSize; i++){
   for(let j = 0; j < canvasSize; j++){
     let newBox = document.createElement("div")
     newBox.classList.add("box");
+    // newBox.classList.add("filled");
     newBox.addEventListener("mouseover", function(){
+      console.log(`drawing = ${drawing}, erasing = ${erasing}`); 
       if(drawing === true){
         newBox.classList.add("filled"); 
+      }
+      else if(erasing === true){
+        newBox.classList.remove("filled");
       }
       
     })
@@ -20,12 +29,30 @@ for(let i = 0; i < canvasSize; i++){
   canvas.appendChild(newRow);
 }
 
-canvas.addEventListener("mousedown", function(){
-  drawing = true;
+drawBtn.addEventListener("click", function(){
+  console.log(`drawing = ${drawing}, erasing = ${erasing}`); 
+  canvas.addEventListener("mousedown", function(){
+    erasing = false;
+    drawing = true;
+    console.log(`drawing = ${drawing}, erasing = ${erasing}`); 
+  })
+  canvas.addEventListener("mouseup", function(){
+    drawing = false;
+    console.log(`drawing = ${drawing}, erasing = ${erasing}`); 
+  })
 })
 
-canvas.addEventListener("mouseup", function(){
-  drawing = false;
+eraseBtn.addEventListener("click", function(){
+  console.log(`drawing = ${drawing}, erasing = ${erasing}`); 
+  canvas.addEventListener("mousedown", function(){
+    drawing = false;
+    erasing = true;
+    console.log(`drawing = ${drawing}, erasing = ${erasing}`); 
+  })
+  canvas.addEventListener("mouseup", function(){
+    erasing = false;
+    console.log(`drawing = ${drawing}, erasing = ${erasing}`); 
+  })
 })
 
 clearBtn.addEventListener("click", function(){
